4e2693bff7e7090a3e17259521f6439aea23eb03
Jose Lane <dariankline@outlook.com>
2024-10-08 15:36:48 +0800

    [module] develop/typescript: refactor to enable prettier

--- modules/develop/typescript.nix
+++ modules/develop/typescript.nix
@@ -1,27 +1,71 @@
 { mkHomeModule, options, ... }:
 {
-  home = mkHomeModule {
-    id = "typescript";
-    name = "TypeScript";
+  home =
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
+    {
+      options = with lib; {
+        develop.typescript = {
+          enable = mkEnableOption "TypeScript environment";
 
-    env.config = { pkgs, ... }: cfg: { home.packages = [ pkgs.typescript ]; };
+          env.enable = options.mkDisableOption "TypeScript build tools";
 
-    editor = {
-      helix.config =
-        { pkgs, ... }:
-        cfg: {
-          languages.language-server.typescript-language-server = {
-            command = "${pkgs.nodePackages.typescript-language-server}/bin/typescript-language-server";
+          editor = {
+            vscode.enable = mkEnableOption "VSCode TypeScript support";
+            helix.enable = mkEnableOption "Helix TypeScript support";
+            nixvim.enable = mkEnableOption "Nixvim TypeScript support";
           };
         };
+      };
 
-      nixvim.config =
-        { pkgs, ... }:
-        cfg: {
-          plugins = {
-            lsp.servers.tsserver.enable = true;
+      config =
+        let
+          cfg = config.develop.typescript;
+        in
+        lib.mkIf cfg.enable {
+          home.packages = lib.mkIf cfg.env.enable [ pkgs.typescript ];
+
+          develop.prettier =
+            let
+              editor = cfg.editor;
+            in
+            {
+              enable = editor.vscode.enable || editor.nixvim.enable;
+              editor = {
+                vscode = lib.mkIf editor.vscode.enable {
+                  enable = true;
+                  languages = {
+                    typescript = true;
+                    typescriptreact = true;
+                  };
+                };
+                nixvim = lib.mkIf editor.nixvim.enable {
+                  enable = true;
+                  languages = {
+                    typescript = true;
+                    typescriptreact = true;
+                  };
+                };
+              };
+            };
+
+          programs.helix = lib.mkIf cfg.editor.helix.enable {
+            languages.language-server.typescript-language-server = {
+              command = "${pkgs.nodePackages.typescript-language-server}/bin/typescript-language-server";
+            };
+          };
+
+          programs.nixvim = lib.mkIf cfg.editor.nixvim.enable {
+            plugins = {
+              lsp.servers.tsserver = {
+                enable = true;
+              };
+            };
           };
         };
     };
-  };
 }
