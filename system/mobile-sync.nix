516b3e89a2536529dbe0ff3d492d851dc0bc9eda
Jose Lane <dariankline@outlook.com>
2024-10-17 23:37:38 +0800

    [system] mobile-sync/ios: add ssh config

--- system/mobile-sync.nix
+++ system/mobile-sync.nix
@@ -3,10 +3,20 @@
     { pkgs, ... }:
     {
       services.usbmuxd.enable = true;
-      environment.systemPackages = [
-        pkgs.ifuse
-        pkgs.libimobiledevice
+      environment.systemPackages = with pkgs; [
+        ifuse
+        libusbmuxd # for port forwarding
+        libimobiledevice
+        sshfs
       ];
+
+      programs.ssh.extraConfig = ''
+        Host ios-usb
+          Hostname 127.0.0.1
+          StrictHostKeyChecking no
+          ProxyCommand ${pkgs.libusbmuxd}/bin/inetcat 22
+          UserKnownHostsFile /dev/null
+      '';
     };
 
   android =
