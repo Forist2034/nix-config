79ebaf744f7ac3ef72f394da2003ea87764d371e
Jose Lane <dariankline@outlook.com>
2024-10-24 19:42:04 +0800

    [module] develop/js: add pnpm

--- modules/develop/javascript.nix
+++ modules/develop/javascript.nix
@@ -10,6 +10,7 @@
     options = {
       enable = lib.mkEnableOption "Javascript";
       yarn.enable = options.mkDisableOption "Yarn";
+      pnpm.enable = options.mkDisableOption "Pnpm";
     };
     config =
       { value, ... }:
@@ -19,6 +20,10 @@
           (lib.mkIf value.yarn.enable [
             ".yarn"
             ".cache/yarn"
+          ])
+          (lib.mkIf value.pnpm.enable [
+            ".cache/pnpm"
+            ".local/share/pnpm/store"
           ])
         ];
       };
@@ -40,6 +45,7 @@
             enable = options.mkDisableOption "JavaScript build tools";
 
             yarn.enable = options.mkDisableOption "Yarn package manager";
+            pnpm.enable = options.mkDisableOption "Pnpm package manager";
           };
 
           editor = {
@@ -57,6 +63,7 @@
           home.packages = lib.mkIf cfg.env.enable [
             pkgs.nodejs
             (lib.mkIf cfg.env.yarn.enable pkgs.yarn)
+            (lib.mkIf cfg.env.pnpm.enable pkgs.pnpm)
           ];
 
           develop.prettier =
