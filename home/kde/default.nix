9a7d66164a940e68d75df729a80f019dbfda91af
Jose Lane <dariankline@outlook.com>
2025-03-20 09:24:37 +0800

    [home] kde: fix system window color

--- home/kde/default.nix
+++ home/kde/default.nix
@@ -47,6 +47,7 @@ in
       # use system font config
       xdg.configFile."kdeglobals".text = ''
         [General]
+        ColorSchemeHash=f13a5f93a8d2186748a87eaf56ec9fcef24073d5
         fixed=Monospace,10,-1,5,400,0,0,0,0,0,0,0,0,0,0,1
         font=Sans Serif,10,-1,5,400,0,0,0,0,0,0,0,0,0,0,1
         menuFont=Sans Serif,10,-1,5,400,0,0,0,0,0,0,0,0,0,0,1
@@ -54,7 +55,13 @@ in
         toolBarFont=Sans Serif,10,-1,5,400,0,0,0,0,0,0,0,0,0,0,1
 
         [WM]
+        activeBackground=227,229,231
+        activeBlend=227,229,231
         activeFont=Sans Serif,10,-1,5,400,0,0,0,0,0,0,0,0,0,0,1
+        activeForeground=35,38,41
+        inactiveBackground=239,240,241
+        inactiveBlend=239,240,241
+        inactiveForeground=112,125,138
       '';
     };
 }
