4710f0a59a9ecd862e52a5451871e7be794849dc
Jose Lane <dariankline@outlook.com>
2025-06-15 10:19:34 +0800

    [home] remove unused gh

--- home/default.nix
+++ home/default.nix
@@ -1,7 +1,6 @@
 {
   firefox = import ./firefox;
   kde = import ./kde;
-  gh = import ./gh.nix;
   starship = import ./starship.nix;
   taskwarrior = import ./taskwarrior.nix;
   helix = import ./helix;
