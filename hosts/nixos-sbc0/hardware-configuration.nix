8203036e3e3951aa063ea2c46fa3dc9970459c6f
Jose Lane <dariankline@outlook.com>
2025-07-19 20:59:02 +0800

    [hosts] nixos-sbc0: set key as power

--- hosts/nixos-sbc0/hardware-configuration.nix
+++ hosts/nixos-sbc0/hardware-configuration.nix
@@ -1,10 +1,34 @@
 { config, pkgs, ... }:
+let
+  dtbFilename = config.hardware.deviceTree.name;
+  dtbFile = "${config.hardware.deviceTree.package}/${dtbFilename}";
+in
 {
   nixpkgs.hostPlatform = {
     system = "armv7l-linux";
   };
 
+  hardware.deviceTree = {
+    enable = true;
+    name = "sun8i-h3-orangepi-lite.dtb";
+    filter = "*orangepi-lite*.dtb";
+    overlays = [
+      {
+        name = "power_key";
+        dtsFile = ./power_key.dts;
+      }
+    ];
+  };
+
   boot.extraModulePackages = with config.boot.kernelPackages; [
     rtl8189fs
   ];
+
+  boot.uki.settings = {
+    UKI = {
+      DeviceTree = dtbFile;
+    };
+  };
+
+  # TODO: add device tree config for systemd-boot
 }
