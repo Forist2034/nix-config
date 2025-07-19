7f9b651008b62cbafb27d46b3ad4b06eabfc8649
Jose Lane <dariankline@outlook.com>
2025-07-19 11:47:18 +0800

    [hosts] nixos-sbc0: add ssh host keys

--- hosts/nixos-sbc0/default.nix
+++ hosts/nixos-sbc0/default.nix
@@ -2,6 +2,14 @@
 {
   system = "armv7l-linux";
 
+  sshConfig = local-lib.ssh.mkHostConfig {
+    name = "nixos-sbc0";
+    hostKeys = [
+      (builtins.readFile ./ssh_host_ed25519_key.pub)
+      (builtins.readFile ./ssh_host_rsa_key.pub)
+    ];
+  };
+
   userPasswordFile = user: "/mnt/config/etc/user-passwords/${user}";
 
   hardware = {
