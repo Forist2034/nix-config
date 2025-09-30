6f2d7a4beb4d771c229d7b8ee34de5be02749803
Jose Lane <dariankline@outlook.com>
2025-09-30 23:31:23 +0800

    [hosts] nixos-tablet0: add markdown support

--- hosts/nixos-tablet0/home.nix
+++ hosts/nixos-tablet0/home.nix
@@ -3,4 +3,14 @@
   imports = [
     suites.develop.home
   ];
+
+  develop = {
+    markdown = {
+      enable = true;
+      editor = {
+        vscode.enable = true;
+        nixvim.enable = true;
+      };
+    };
+  };
 }
