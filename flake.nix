3e7e9287f19da9aef72b92a5f9024e156568d96b
Jose Lane <dariankline@outlook.com>
2026-03-25 08:29:21 +0800

    add private config as submodule

--- flake.nix
+++ flake.nix
@@ -26,10 +26,12 @@
     };
 
     private-config = {
-      type = "git";
-      url = "file:///etc/nixos/private";
-      rev = "09bb1953fa20ad7797b00c58644e332a71f80553";
+      # need to set remote to absolute path of a local git repo to make submodule
+      # relative path resolve to a absolute local directory
+      url = "path:private";
     };
+
+    self.submodules = true;
   };
 
   outputs =
