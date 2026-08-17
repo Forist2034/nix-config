14cca78248c060110d5202838d1d6664fedbf03a
Jose Lane <dariankline@outlook.com>
2026-08-18 06:50:20 +0800

    [parts] direnv: load env file

--- parts/direnv.nix
+++ parts/direnv.nix
@@ -3,6 +3,11 @@
     default = { ... }: {
       programs.direnv = {
         enable = true;
+        config = {
+          global = {
+            load_dotenv = true;
+          };
+        };
       };
     };
   };
